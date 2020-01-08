49f551c0cef87d1ba2ec01f04f8cc4db3a672044
hlfbprince <batuhankesim12@gmail.com>
2020-01-08 21:05:58 +0300

    Lesson 3- Model Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todoApp';
+  user = 'Batuhan';
+  items= [
+    { description: "Kahvaltı", action: "No" },
+    { description: "Sinema", action: "No" },
+    { description: "Spor", action: "No" },
+    { description: "Fatura", action: "No" },
+
+  ];
 }
